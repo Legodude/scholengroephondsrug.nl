c3485453c6c26cf98383d6af2ffbb25ee54c847a
nickklein <github@nickklein.nl>
2014-06-24 19:57:22 +0200

    useradd werkt
    kleine security fix mbt dropdowngenerator

--- inc/database.inc.php
+++ inc/database.inc.php
@@ -2,7 +2,7 @@
 
     $hostname="beheer.scholengroephondsrug.nl";
     $username="root";
-    $password="";
+    $password="1biertje";
     $database="cmdb";
         $port="6612";
 
--- pages/users.page.php
+++ pages/users.page.php
@@ -1,27 +1,23 @@
-s<?php
-if(!isset($_POST['submit']))
-{    
-    echo '<form "action="index.php?action=# METHOD="POST">
+<?php
+    echo '
+        <form action="#" METHOD="POST">
     <table>
         <tr>
-            <td><label for="voornaam">Voornaam</label></td><td><input type="text" name="voornaam" /></td>
+            <td><label for="voornaam">Voornaam:</label></td><td><input autocomplete="off" type="text" name="voornaam" /></td>
         </tr>
         <tr>
-            <td><label for="achternaam">Achternaam</label></td><td><input type="text" name="achternaam" /></td>
+            <td><label for="achternaam">Achternaam:</label></td><td><input autocomplete="off" type="text" name="achternaam" /></td>
         </tr>
         <tr>
-            <td><label for="email">E-mail</label></td><td><input type="text" name="email" /></td>
+            <td><label for="email">E-mail:</label></td><td><input autocomplete="off" type="text" name="email" /></td>
         </tr>
         <tr>
-            <td><label for="wachtwoord">Wachtwoord</label></td><td><input type="password" name="wachtwoord" /></td>
+            <td><label for="wachtwoord">Wachtwoord:</label></td><td><input autocomplete="off" type="password" name="wachtwoord" /></td>
         </td>
     </table>
         <input type="submit" value="Aanmaken" />
     </form> ';
-}
-else
-{
-    if(!isset($_POST['gebruikerscode'])&&($_POST['wachtwoord'])&&($_POST['voornaam'])&&($_POST['achternaam'])&&($_POST['email']))
+    /*if(!isset($_POST['gebruikerscode'])&&($_POST['wachtwoord'])&&($_POST['voornaam'])&&($_POST['achternaam'])&&($_POST['email']))
     {
         echo "De velden zijn niet volledig ingevuld";
         echo "De velden zijn met de volgende informatie gevuld.'<br>'";
@@ -39,9 +35,17 @@ else
         $email = $_POST['email'];
         
         $gebruikercode = strtoupper(substr($achternaam ,0,3).substr($voornaam,0,2));
+    }*/
+    if(isset($_POST['wachtwoord'])&&isset($_POST['voornaam'])&&isset($_POST['achternaam'])&&isset($_POST['email']))
+    {        
+        $wachtwoord = $mysqli->real_escape_string(strip_tags($_POST['wachtwoord']));
+        $voornaam = $mysqli->real_escape_string(strip_tags($_POST['voornaam']));
+        $achternaam = $mysqli->real_escape_string(strip_tags($_POST['achternaam']));
+        $email = $mysqli->real_escape_string(strip_tags($_POST['email']));
         
-        
+        $gebruikercode = strtoupper(substr($achternaam ,0,3).substr($voornaam,0,2));
+        $sql = "INSERT INTO `cms_gebruikers`(gebruikercode,wachtwoord,voornaam,achternaam,email)
+                        VALUES('$gebruikercode', '$wachtwoord', '$voornaam', '$achternaam', '$email')";
+        $result =  $mysqli->query($sql);
     }
-}
-
 ?>
