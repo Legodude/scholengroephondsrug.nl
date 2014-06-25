bb064d097d940769b142497805b83549857c9990
GaZzNL <robert-stocker@hotmail.com>
2014-06-25 17:18:22 +0200

    userchange almost done
    blablabliebla

--- pages/changeUser.php
+++ pages/changeUser.php
@@ -1,32 +1,58 @@
-<!-- echo USER_LEVEL_EVERYONE;-->
-<?php $sql = 'SELECT * FROM `cms_gebruikers`';
-$result = $mysqli->query($sql);
-$userlist = array();
-if($rusult =="1")
-{
-    
-}
+<?php
+/*
+$voornaam = $_POST['voornaam'];
+$achternaam = $_POST['achternaam'];
+$email = $_POST['email'];
+$wachtwoord = $_POST['wachtwoord'];
+$acountlevel = $_POST['acountlevel'];
+*/
+$voornaam = "robert";
+$achternaam = "stocker";
+$email = "test@test.local";
+$wachtwoord = "1biertje";
+$acountlevel = 1;
 ?>
 <form action="#" METHOD="POST">
     <table>
         <tr>
-            <td><label for="voornaam">Voornaam:</label></td><td><input autocomplete="off" type="text" name="voornaam" /></td>
+            <td>Voornaam:</td><td><input autocomplete="off" type="text" name="voornaam" value="<?php echo $voornaam; ?>" /></td>
         </tr>
         <tr>
-            <td><label for="achternaam">Achternaam:</label></td><td><input autocomplete="off" type="text" name="achternaam" /></td>
+            <td>Achternaam:</td><td><input autocomplete="off" type="text" name="achternaam" value="<?php echo $achternaam; ?>" /></td>
         </tr>
         <tr>
-            <td><label for="email">E-mail:</label></td><td><input autocomplete="off" type="text" name="email" /></td>
+            <td>E-mail:</td><td><input autocomplete="off" type="text" name="email" value="<?php echo $email; ?>" /></td>
         </tr>
         <tr>
-            <td><label for="wachtwoord">Wachtwoord:</label></td><td><input autocomplete="off" type="password" name="wachtwoord" /></td>
+            <td>Wachtwoord:</td><td><input autocomplete="off" type="text" name="wachtwoord" value="<?php echo $wachtwoord; ?>"/></td>
         </tr>
         <tr>
-            <td>Acountlevel:</td><td><select><?php ?>
+            <td>Acountlevel:</td><td><select>
+                    <option value="<?phpecho$acountlevel;?>">Level nu: <?php echo$acountlevel;?>"/></option>
+                    <option> value""<?php echo USER_LEVEL_EVERYONE;?></option>
+                </select>
+            </td>
+        </tr>
     </table>
-    <input type="submit" value="Aanmaken" />
+    <input type="submit" value="Aanpassen" />
 </form>
 <?php
+if(isset($_POST['wachtwoord'])&&isset($_POST['voornaam'])&&isset($_POST['achternaam'])&&isset($_POST['email']))
+{
+ 
+    $voornaam = $_POST['voornaam'];
+    $achternaam = $_POST['achternaam'];
+    $email = $_POST['email'];
+    $wachtwoord = $_POST['wachtwoord'];
+    $acountlevel = $_POST['acountlevel'];
+    
+    $sql = 'UPDATE ``';
+    
+    foreach ($_POST as $key => $value) {
 
+        echo $value."<br>";
+        
+    }
+}
 
 ?>
