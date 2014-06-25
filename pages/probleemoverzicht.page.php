fc587c9a13dde431937c319c50c8d165c46f3372
Figueus <j-c-s-s@hotmail.com>
2014-06-25 22:38:14 +0200

    Probleemoverzicht
    klikbare rows

--- pages/probleemoverzicht.page.php
+++ pages/probleemoverzicht.page.php
@@ -4,9 +4,18 @@ $query="SELECT Probleem_ID, Probleem_Omschrijving, Probleem_Workaround, Probleem
         FROM im_problemen";
 $result=$mysqli->query($query);
 echo "<table><tr><th>ID</th><th>Omschrijving</th><th>Workaround</th><th>Opgelost</th><th>Fout</th></tr>";
-while($row = $result->fetch_assoc())
-{
-print("<tr><td>$row[Probleem_ID]</td><td>$row[Probleem_Omschrijving]</td><td>$row[Probleem_Workaround]</td><td>$row[Probleem_Opgelost]</td><td>$row[Fout]</td></tr>");
+$i=0;
+while($row=$result->fetch_assoc()){
+    echo '<tr onclick="window.document.location.href=\'?action=probleemoverzicht&probleemID='.$row["Probleem_ID"].'\'" class="ci-table-row ';
+        if($i%2) echo "even";
+        else echo "uneven";
+        echo '">';
+        foreach($row as $field)
+        {
+            echo '<td class="ci-table-cell">'.$field."</td>";
+        }
+        echo "</tr>";
+        $i++;
 }
 echo "</table>";
 ?>
