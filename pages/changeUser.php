58fff15155bf90cffefa1618c0436b8a77d71afa
GaZzNL <robert-stocker@hotmail.com>
2014-06-25 13:52:23 +0200

    changeuser
    Deze page is nu aangemaakt

---
+++ pages/changeUser.php
@@ -0,0 +1,24 @@
+<!-- echo USER_LEVEL_EVERYONE;-->
+<form action="#" METHOD="POST">
+    <table>
+        <tr>
+            <td><label for="voornaam">Voornaam:</label></td><td><input autocomplete="off" type="text" name="voornaam" /></td>
+        </tr>
+        <tr>
+            <td><label for="achternaam">Achternaam:</label></td><td><input autocomplete="off" type="text" name="achternaam" /></td>
+        </tr>
+        <tr>
+            <td><label for="email">E-mail:</label></td><td><input autocomplete="off" type="text" name="email" /></td>
+        </tr>
+        <tr>
+            <td><label for="wachtwoord">Wachtwoord:</label></td><td><input autocomplete="off" type="password" name="wachtwoord" /></td>
+        </tr>
+        <tr>
+            <td>Acountlevel:</td><td><input type="radio" 
+    </table>
+    <input type="submit" value="Aanmaken" />
+</form>
+<?php
+
+
+?>
