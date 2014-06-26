4f32537424fac612d2f1ce280e716b6ea4dd32bf
GaZzNL <robert-stocker@hotmail.com>
2014-06-26 15:31:09 +0200

    changeuser.page
    DONE!

--- pages/changeuser.page.php
+++ pages/changeuser.page.php
@@ -1,16 +1,18 @@
 <?php
-/*
-$voornaam = $_POST['voornaam'];
-$achternaam = $_POST['achternaam'];
-$email = $_POST['email'];
-$wachtwoord = $_POST['wachtwoord'];
-$acountlevel = $_POST['acountlevel'];
-*/
-$voornaam = "robert";
-$achternaam = "stocker";
-$email = "test@test.local";
-$wachtwoord = "1biertje";
-$acountlevel = 1;
+
+$gebruikercode = $_POST['gebruikercode'];
+
+$sql = 'SELECT * 
+FROM `cms_gebruikers`
+WHERE `gebruikercode` = "'.$gebruikercode.'"';
+
+$user = $mysqli->query($sql);
+$userinfo = array();
+
+if($result->num_rows==1)
+{
+    $userinfo = $result->fetch_assoc(); 
+
 
 switch ($acountlevel)
 {
@@ -40,20 +42,20 @@ switch ($acountlevel)
 <form action="#" METHOD="POST">
     <table>
         <tr>
-            <td>Voornaam:</td><td><input autocomplete="off" type="text" name="voornaam" value="<?php echo $voornaam; ?>" /></td>
+            <td>Voornaam:</td><td><input autocomplete="off" type="text" name="voornaam" value="<?php echo $voornaam['voornaam']; ?>" /></td>
         </tr>
         <tr>
-            <td>Achternaam:</td><td><input autocomplete="off" type="text" name="achternaam" value="<?php echo $achternaam; ?>" /></td>
+            <td>Achternaam:</td><td><input autocomplete="off" type="text" name="achternaam" value="<?php echo $achternaam['achternaam']; ?>" /></td>
         </tr>
         <tr>
-            <td>E-mail:</td><td><input autocomplete="off" type="text" name="email" value="<?php echo $email; ?>" /></td>
+            <td>E-mail:</td><td><input autocomplete="off" type="text" name="email" value="<?php echo $email['email']; ?>" /></td>
         </tr>
         <tr>
-            <td>Wachtwoord:</td><td><input autocomplete="off" type="text" name="wachtwoord" value="<?php echo $wachtwoord; ?>"/></td>
+            <td>Wachtwoord:</td><td><input autocomplete="off" type="text" name="wachtwoord" value="<?php echo $wachtwoord['wachtwoord']; ?>"/></td>
         </tr>
         <tr>
             <td>Acountlevel:</td><td><select name="acountlevel">
-                    <option value="<?php echo $acountlevel;?>">Rechten nu: <?php echo$acountlevelnaam;?></option>
+                    <option value="<?php echo $acountlevel['gebruikerslevel'];?>">Rechten nu: <?php echo$acountlevelnaam;?></option>
                     <option value="0">EVERYONE</option>
                     <option value="1">INCIDENTBEHEER</option>
                     <option value="2">PROBLEEMBEHEER</option>
@@ -65,8 +67,15 @@ switch ($acountlevel)
         </tr>
     </table>
     <input type="submit" value="Aanpassen" />
+    <input type="hidden" name="gebruikerscode" value="<?php echo $gebruikerscode;?>">
 </form>
 <?php
+}
+else
+{
+    echo "geen gebruiker informatie gevonden";
+}
+
 if(isset($_POST['wachtwoord'])&&isset($_POST['voornaam'])&&isset($_POST['achternaam'])&&isset($_POST['email']))
 {
  
@@ -75,8 +84,13 @@ if(isset($_POST['wachtwoord'])&&isset($_POST['voornaam'])&&isset($_POST['achtern
     $email = $_POST['email'];
     $wachtwoord = $_POST['wachtwoord'];
     $acountlevel = $_POST['acountlevel'];
+    $gebruikercode = $_POST['gebruikerscode'];
     
-    $sql = 'UPDATE ``';
+    $sql = 'UPDATE `cms_gebruikers`
+            SET `voornaam` = "'.$voornaam.', `achternaam` = "'.$achternaam.'", `email`="'.$email.'" `wachtwoord` = "'.$wachtwoord.'", `gebruikreslevel` = "'.$acountlevel.'"
+            WHERE gebruikerscode = "'.$gebruikerscode.'"';
+    
+    $result =  $mysqli->query($sql);
     
     foreach ($_POST as $key => $value) {
 
@@ -84,5 +98,4 @@ if(isset($_POST['wachtwoord'])&&isset($_POST['voornaam'])&&isset($_POST['achtern
         
     }
 }
-
 ?>
