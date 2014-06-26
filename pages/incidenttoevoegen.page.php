c8db9ea69b399c739cc3326e2e427cc79af55eb2
nickklein <github@nickklein.nl>
2014-06-26 12:17:19 +0200

    laatste debugs weghalen

--- pages/incidenttoevoegen.page.php
+++ pages/incidenttoevoegen.page.php
@@ -1,4 +1,21 @@
 <?php
+if(isset($_POST['Hardware_ID']))
+{
+    $sql = 'INSERT INTO `im_incidenten` 
+            (IncidentHardware_ID,IncidentAanvang,IncidentOmschrijving,IncidentWorkaround,IncidentImpact,IncidentUrgentie,gebruikercode,IncidentStatus)
+            VALUES
+            ("'.$_POST['Hardware_ID'].'",
+            "'.$_POST['incidentAanvangdatum'].' '.$_POST['incidentAanvangtijd'].'",
+            "'.$_POST['incidentOmschrijving'].'",
+            "'.$_POST['incidentWorkaround'].'",
+            "'.$_POST['incidentImpact'].'",
+            "'.$_POST['incidentUrgentie'].'",
+            "'.$_POST['gebruikercode'].'",
+            "'.$_POST['incidentStatus'].'")';
+    echo "Incident aangemaakt!";
+    
+    exit();
+}
 
 if(!isset($_GET['hardwareID']))
 {
@@ -9,8 +26,9 @@ else
     ?>
     <form action="#" method="POST" >
         <table>
-            <tr><td>HardwareID:</td><td><input type="text" disabled="disabled" name="Hardware_ID" <?php echo 'value="'.$_GET['hardwareID'].'"'; ?> /></td></tr>
-            <tr><td>Aanvang:</td><td><input type="datetime" name="incidentAanvang" /></td></tr>
+            <tr><td>HardwareID:</td><td><input type="text" readonly="readonly" name="Hardware_ID" <?php echo 'value="'.$_GET['hardwareID'].'"'; ?> /></td></tr>
+            <tr><td>Aanvangdatum:</td><td><input type="date" name="incidentAanvangdatum" /></td></tr>
+            <tr><td>Aanvangtijd:</td><td><input type="text" name="incidentAanvangtijd" /></td></tr>
             <tr><td>Omschrijving:</td><td><input type="text" name="incidentOmschrijving" /><br />
             <tr><td>Workaround:<br />(mits van toepassing)</td><td><input type="text" name="incidentWorkaround" /><br />
             <tr><td>Impact:</td><td><select name="incidentImpact" />
@@ -23,8 +41,8 @@ else
                 <option value="2">Gemiddeld</option>
                 <option value="1">Laag</option>
             </select></td></tr>
-            <tr><td>Gebruiker:</td><td><input type="text" disabled="disabled" value="<?php echo $_SESSION['gebruikercode']; ?>" /></td></tr>
-            <tr><td>Status:</td><td><select name="incidentUrgentie" />
+            <tr><td>Gebruiker:</td><td><input name="gebruikercode" type="text" readonly="readonly" value="<?php echo $_SESSION['gebruikercode']; ?>" /></td></tr>
+            <tr><td>Status:</td><td><select name="incidentStatus" />
                 <option value="1">Open</option>
                 <option value="2">In behandeling</option>
                 <option value="3">Gesloten</option>
