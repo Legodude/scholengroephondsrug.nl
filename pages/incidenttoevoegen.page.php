e6464c44dbd2b9707fec0bc747e63ebd3827730f
nickklein <github@nickklein.nl>
2014-06-26 20:48:20 +0200

    fixes

--- pages/incidenttoevoegen.page.php
+++ pages/incidenttoevoegen.page.php
@@ -1,3 +1,13 @@
+<script type="text/javascript">
+    function addOmschrijvingBox()
+    {
+        var dropdown = document.getElementById('omschrijvingdropdown');
+        var textfield = document.getElementById('omschrijvingtext');
+        dropdown.name="omschrijvinginvisible";
+        textfield.name="omschrijving";
+        textfield.style.display="block";
+    }
+</script>
 <?php
 if(isset($_POST['Hardware_ID']))
 {
@@ -29,8 +39,21 @@ else
             <tr><td>HardwareID:</td><td><input type="text" readonly="readonly" name="Hardware_ID" <?php echo 'value="'.$_GET['hardwareID'].'"'; ?> /></td></tr>
             <tr><td>Aanvangdatum:</td><td><input type="date" name="incidentAanvangdatum" /></td></tr>
             <tr><td>Aanvangtijd:</td><td><input type="text" name="incidentAanvangtijd" /></td></tr>
-            <tr><td>Omschrijving:</td><td><input type="text" name="incidentOmschrijving" /><br />
-            <tr><td>Workaround:<br />(mits van toepassing)</td><td><input type="text" name="incidentWorkaround" /><br />
+            <tr><td>Omschrijving:</td>
+                <td id="omschrijvingcell">
+                    <select id="omschrijvingdropdown" name="omschrijving" style="width:155px;">
+                        <?php
+                        $sql = 'SELECT DISTINCT `IncidentOmschrijving` from `im_incidenten`';
+                        $result = $mysqli->query($sql);
+                        while($row = $result->fetch_assoc())
+                        {
+                            echo '<option>'.$row['IncidentOmschrijving'].'</option>';
+                        }
+                        ?>
+                    </select><a style="font-size: 12px;" onclick="addOmschrijvingBox();" >Of voeg een nieuwe omschrijving toe</a>
+                    <input placeholder="Omschrijving:" style="display:none;" type="text" name="omschrijvinginvisible" id="omschrijvingtext" />
+                </td></tr>
+            <tr><td>Workaround:<br />(mits van toepassing)</td><td><input type="text" name="incidentWorkaround" /></td></tr>
             <tr><td>Impact:</td><td><select name="incidentImpact" />
                 <option value="3">Hoog</option>
                 <option value="2">Gemiddeld</option>
