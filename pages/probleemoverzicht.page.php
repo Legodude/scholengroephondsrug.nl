b67faea21e82063de80d01045254392d46fa4e9d
nickklein <github@nickklein.nl>
2014-06-26 22:07:13 +0200

    laatste push

--- pages/probleemoverzicht.page.php
+++ pages/probleemoverzicht.page.php
@@ -1,22 +1,25 @@
 <html>
 <?php
-$query="SELECT Probleem_ID, Probleem_Omschrijving, Probleem_Workaround, Probleem_Opgelost, Fout
+$query="SELECT Probleem_ID, Probleem_Omschrijving, Probleem_Workaround, Probleem_Opgelost
         FROM im_problemen";
 $result=$mysqli->query($query);
 echo "<table><tr><th>ID</th><th>Omschrijving</th><th>Workaround</th><th>Opgelost</th><th>Fout</th></tr>";
 $i=0;
-while($row=$result->fetch_assoc()){
-    echo '<tr onclick="window.document.location.href=\'?action=probleemoverzicht&probleemID='.$row["Probleem_ID"].'\'" class="ci-table-row ';
-        if($i%2) echo "even";
-        else echo "uneven";
-        echo '">';
-        foreach($row as $field)
-        {
-            echo '<td class="ci-table-cell">'.$field."</td>";
-        }
-        echo "</tr>";
-        $i++;
-}
-echo "</table>";
+if($result->num_rows!=0)
+{
+    while($row=$result->fetch_assoc()){
+        echo '<tr  class="ci-table-row ';
+            if($i%2) echo "even";
+            else echo "uneven";
+            echo '">';
+            foreach($row as $field)
+            {
+                echo '<td class="ci-table-cell">'.$field."</td>";
+            }
+            echo "</tr>";
+            $i++;
+    }
+    echo "</table>";
+} else echo "geen problemen.";
 ?>
 </html>
